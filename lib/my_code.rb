0f4b2eda58687ad4dc68485f681a365119f0c077
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-23 03:48:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+def map(arr)
+  result = []
+  arr.length.times {|i|
+    result.push(yield(arr[i]))
+  }
+  return result
+end
+
+def reduce(arr, acc=nil) 
+  arr.length.times {|i|
+    acc = yield(acc, arr[i])
+  }
+  return acc
+end
+  
